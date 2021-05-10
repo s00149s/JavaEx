b7cc3a4a77369c2f3ecffa7dd33339c0f424229d
bit <bit@DESKTOP-60QQ7IK>
2021-05-10 09:22:49 +0900

    StringBuffer

--- JavaEx/src/com/javaex/api/stringclass/StringEx.java
+++ JavaEx/src/com/javaex/api/stringclass/StringEx.java
@@ -3,8 +3,40 @@ package com.javaex.api.stringclass;
 public class StringEx {
 
 	public static void main(String[] args) {
-		stringBasic();
-		usefulMethods();
+//		stringBasic();
+//		usefulMethods();
+		stringBufferEx();
+	}
+	
+	//	문자열 객체 : 연결, 메서드 수행시 매번 새 String 반환
+	//	StringBuffer : 버퍼 기반으로 작동 -> 복잡한 문자열을 만들어내고자 할 때 좀 더 메모리 효율이 좋음
+	private static void stringBufferEx() {
+		//	버퍼 생성(버퍼 길이 10, 실제 길이 4로 자동생성됨)
+		StringBuffer sb = new StringBuffer("This");
+		
+		//	문자열 추가 : append
+		sb.append(" is pencil");
+		//	문자열 삽입 : insert
+		sb.insert(8, "my");
+		//	문자열 치환 : replace
+		sb.replace(8,10,"your ");
+		
+		//	프린트 문으로 출력되기 전까지 객체 생성X
+		System.out.println(sb);
+		
+		//	버퍼 사이즈 변경
+		sb.setLength(10); 	//	내부 버퍼의 크기를 10으로 줄인다
+		System.out.println(sb);
+		
+		//	StringBuffer의 메서드들은 연속해서 호출 -> 메서드 체이닝
+		StringBuffer sb2 = new StringBuffer("This")
+				.append(" is pencil")
+				.insert(8, "my")
+				.replace(8,  10, "your ");
+		String s = sb2.toString();  	//	최종 변환
+		
+		System.out.println("결과 : " + s);
+				
 	}
 	
 	private static void stringBasic() {
